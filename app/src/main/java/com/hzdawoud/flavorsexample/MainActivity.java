af651c7243ad20e6de44ef284cb9abbe5bb19151
hzdawoud <hzdawoud@gmail.com>
2021-01-16 11:07:00 +0200

    first commit

---
+++ app/src/main/java/com/hzdawoud/flavorsexample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.hzdawoud.flavorsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        btn.setBackground(getDrawable(R.drawable.shape_button_primary));
+    }
+}
